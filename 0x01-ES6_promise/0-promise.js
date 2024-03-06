0b941894b45786c35b8fd93fc755f0ddb50539ca
sandra113 <sandramabonga303@gmail.com>
2024-03-06 20:22:11 +0000

    promises

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,12 +1,12 @@
 export default function getResponseFromAPI() {
-    return new Promise((resolve, reject) => {
-        // Simulating API call
-        setTimeout(() => {
-            // Assuming API call is successful
-            const responseData = { /* Your API response data */ };
-            resolve(responseData);
-            // If there's an error, you can reject the promise
-            // reject(new Error("API call failed"));
-        }, 1000); // Simulating a 1-second delay
-    });
+  return new Promise((resolve) => {
+    // Simulating API call
+    setTimeout(() => {
+      // Assuming API call is successful
+      const responseData = { /* Your API response data */ };
+      resolve(responseData);
+      // If there's an error, you can reject the promise
+      // reject(new Error("API call failed"));
+    }, 1000); // Simulating a 1-second delay
+  });
 }
