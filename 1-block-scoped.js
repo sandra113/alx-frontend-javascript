9015fc2ecc9a89a6b1a740ab15fa2ffef078d496
sandra113 <sandramabonga303@gmail.com>
2024-03-08 07:52:01 +0000

    hoisting

---
+++ 1-block-scoped.js
@@ -0,0 +1,13 @@
+export default function taskBlock(trueOrFalse){
+	(function() {
+		var task = false;
+		var task2 = true;
+
+		if (trueOrFalse) {
+			task = true;
+			task2 = false;
+		}
+	})();
+
+	return [task, task2];
+}
