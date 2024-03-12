3630b045087f160008a22df34e6b2cbd6dd27aa8
sandra113 <sandramabonga303@gmail.com>
2024-03-12 11:13:18 +0000

    ESlint

--- 0x00-ES6_basic/8-getBudgetCurrentYear.js
+++ 0x00-ES6_basic/8-getBudgetCurrentYear.js
@@ -2,7 +2,6 @@ function getCurrentYear() {
   const date = new Date();
   return date.getFullYear();
 }
-
 export default function getBudgetForCurrentYear(income, gdp, capita) {
   return{
     [`income-${getCurrentYear()}`]: income,
