a58602b10f89ee441f83799bd98c220d0dbe91a2
sandra113 <sandramabonga303@gmail.com>
2024-03-12 09:35:27 +0000

    arrow syntax

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -1,6 +1,5 @@
 export default function getNeighborhoodsList() {
 	this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
-
 	this.addNeighborhood = (newNeighborhood) => {
 		this.sanFranciscoNeighborhoods.push(newNeighborhood);
 		return this.sanFranciscoNeighborhoods;
