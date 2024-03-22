61ec586628a9e892adef35387b21139baeb194b9
sandra113 <sandramabonga303@gmail.com>
2024-03-22 15:45:44 +0000

    classroom

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,12 +1,6 @@
-import ClassRoom from './0-classroom.js';
+import ClassRoom from './0-classroom';
 
-function initializeRooms() {
-	const rooms = [
-		new ClassRoom(19),
-		new ClassRoom(20),
-		new ClassRoom(34
-	];
-			return rooms;
+export default function initializeRooms() {
+  const rooms = [new ClassRoom(19), new ClassRoom(20), new ClassRoom(34)];
+  return rooms;
 }
-
-export { initializeRooms };
